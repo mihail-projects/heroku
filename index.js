e62aa3ea7ac10afef7f255e254564f210e874765
rip <mihalisp@protonmail.com>
2022-03-18 03:19:17 +0200

    -

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
+    console.log(`Listening on port ${process.env.PORT}`)
 })
