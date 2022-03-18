2c0ef7c3db8d63f06c50d8d8b50f57000cc22000
rip <mihalisp@protonmail.com>
2022-03-18 02:39:39 +0200

    port fix

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from 'express'
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
 })
