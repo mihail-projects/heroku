9182082f71bde8459a3c69106bcc193794d67339
rip <mihalisp@protonmail.com>
2022-03-30 00:21:35 +0300

    -

--- index.js
+++ index.js
@@ -2,7 +2,37 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('sucky sucky 30 bucky?')
+
+    var axios = require('axios');
+
+    var data = JSON.stringify({
+        "collection": "collection",
+        "database": "database",
+        "dataSource": "Cluster0",
+        "projection": {
+            "_id": 1
+        }
+    });
+
+    var config = {
+        method: 'post',
+        url: 'https://data.mongodb-api.com/app/data-vlnfz/endpoint/data/beta/action/findOne',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Request-Headers': '*',
+            'api-key': 'Nm52Pf1FE2JPMpyfdO4HL4nMPWSp6BDbSdbWv8Kdg6wuOqSokthyDuqyDKB2jjYG'
+        },
+        data: data
+    };
+
+    axios(config)
+        .then(function (response) {
+            res.send(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            res.send(error);
+        });
+
 })
 
 app.listen(process.env.PORT, () => {
