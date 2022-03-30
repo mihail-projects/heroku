4a40329c71e71d7f81e35db48f986a79d4e4223d
rip <mihalisp@protonmail.com>
2022-03-30 12:00:40 +0300

    -

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 
     client.connect(err => {
         const collection = client.db("database").collection("collection");
-        res.send(collection.dbName)
+        res.send(collection.estimatedDocumentCount())
         client.close();
     });
 
